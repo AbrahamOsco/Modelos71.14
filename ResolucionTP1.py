5b6ddd711994b276839a0f6df672c05c4b0f5b1c
Abraham osco <aosco@fi.uba.ar>
2021-09-17 11:09:16 -0300

    Agregando archivos de consigna y posible resolucion

---
+++ ResolucionTP1.py
@@ -0,0 +1,3 @@
+#Enunciado
+#Una lavandería tiene que lavar prendas, algunas pueden ir juntas y otras no (destiñen).
+#El tiempo de cada lavado es el tiempo que lleva lavar la prenda más sucia de ese lavado.
