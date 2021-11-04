5826ffe068dd580c4f0d81f2c1e58eedf35a841b
AbrahamOsco <aosco@fi.uba.ar>
2021-11-03 21:29:06 -0300

    Se pasa el codigo a POO, ademas se encara usando incompatibilidades

--- ResolucionTP1.py
+++ ResolucionTP1.py
@@ -1,174 +1,160 @@
 #Lee los archivos y devuelve dos diccionarios uno para las prendas incompatibles
 #Y otro diccionario para el tiempo que demora cada prenda 
-import numpy as np
-import pulp
+class Prenda:
+
+	def __init__(self,nroPrenda):
+		self.nro = nroPrenda
+		self.tiempoLavado = 0
+		self.prendasIncomp = []
+		self.EstaEnUnlavado = False
+
+	def definirTiempo(self,unTiempo):
+		self.tiempoLavado=unTiempo
+
+	def addIncompatibilidad(self,prendaIncompatible):
+		if prendaIncompatible not in self.prendasIncomp:
+			self.prendasIncomp.append(prendaIncompatible)
+	
+	def imprimirPrenda(self):
+		print(self.nro,end=':')
+		print(len(self.prendasIncomp),end='\n')
+		#print((self.prendasIncomp),end='')
+		#print(self.tiempoLavado,end='\t \n')
+
+	def getPrendasIncomp(self):
+		return self.prendasIncomp
+
+	def estaEnListaIncomp(self,prenda):
+		return (prenda in self.prendasIncomp)
+	
+	def getNro(self):
+		return self.nro
+	
+	#Se usa para definir como se compara en el sorted
+	def __gt__(self,prenda):
+		return ( len(self.prendasIncomp) > (len(prenda.prendasIncomp) ))
+
+	def cantidadIncom(self):
+		return len(self.prendasIncomp)
+
+	def getTiempo(self):
+		return self.tiempoLavado
+
+class Lavado:
+
+	def __init__(self,nroDeLavado):
+		self.nroLavado=nroDeLavado
+		self.prendas = []
+
+	def agregarPrendaAlLavado(self,unaPrenda):
+		if(unaPrenda not in (self.prendas) and self.sePuedeAgregarPrenda(unaPrenda) ):
+			self.prendas.append(unaPrenda)
+
+	def sePuedeAgregarPrenda(self,unaPrenda):
+		for prenda in self.prendas:
+			if(unaPrenda.getNro() in prenda.getPrendasIncomp()):
+				return False
+		return True
+
+	def imprimirLavado(self):
+		print(self.nroLavado,end=':')
+		for prenda in self.prendas:
+			prenda.imprimirPrenda()
+
+	def getListaPrendas(self):
+		return self.prendas
+
+	def getNroLavado(self):
+		return self.nroLavado
 
 def leerArchivos():
 	archivo = open("ConsignaTP1.py","r")
-	valores = []
 	linea = archivo.readline()
-	dicIncompatible={}
-	dicTiempos = {}
-	
+	listaIncomp = []	
 	while(linea!=""):
 		linea= (linea.strip()).split()
 		if(linea[0] == 'p'):
-			inicializarDiccionarios(linea,dicIncompatible,dicTiempos)
-		elif(linea[0] == 'e'):
-			agregarDiccionario(linea[0],dicIncompatible, int (linea[1]), int (linea[2]) )   
+			inicializarLista(linea,listaIncomp)
+		if(linea[0] == 'e'):						
+			agregarIncomp(listaIncomp,int (linea[1]), int (linea[2]) )   
 		elif(linea[0] =='n'):
-			agregarDiccionario(linea[0],dicTiempos,int (linea[1]),int( linea[2]))
+			listaIncomp[int (linea[1])-1].definirTiempo( int (linea[2])  )
 		linea = archivo.readline()
 
-	return dicIncompatible,dicTiempos
+	return listaIncomp
 
-def agregarDiccionario(id,diccionario,clave,valor,):
-	if(id=='e'):
-		diccionario.get(clave).append(valor)
-	if(id=='n'):
-		diccionario[clave] = valor
+def agregarIncomp(listaIncomp,valor1,valor2,):
+	#Agregamos en ambos sentidos la incompatibilidad de la prenda
+	listaIncomp[valor1-1].addIncompatibilidad(valor2)
+	listaIncomp[valor2-1].addIncompatibilidad(valor1)
 
-#Funcion auxiliar para inicializar los diccionarios.
-def inicializarDiccionarios(linea,dicIncompatible,dicTiempos):
+
+def inicializarLista(linea,listaIncomp):
 	cantidadPrendas = int (linea[2]) #Esto si . 
 	for i in range(1,cantidadPrendas+1):
-		dicIncompatible[i]=[]
-		dicTiempos[i]=0
+		listaIncomp.append( Prenda(i))
 
-#Dado un valor obtengo la clave del diccionario
-def obtengoClaveDadoElValor(unDiccionario,listaIncom):
-	for clave,valor in unDiccionario.items():
-		if(listaIncom == valor):
-			return int (clave) 
-	return 0
 
-def prendaQueMasTardaEnLavar(dicTiempos): 
-	listaAux = []
-	max_actual = max(dicTiempos.values() )
-	prenda = obtengoClaveDadoElValor(dicTiempos,max_actual)
-	dicTiempos.pop(prenda);
-	return prenda 
+def listaPrendasConMasIncomp(listaIncomp):
+	listCantiIncomp = (sorted(listaIncomp,reverse=True))
+	i = 0
+	for prenda in listCantiIncomp:
+		if(i<len(listCantiIncomp)-1):
+			if(listCantiIncomp[i].cantidadIncom() == listCantiIncomp[i+1].cantidadIncom() ):
+				if(listCantiIncomp[i].getTiempo() < listCantiIncomp[i+1].getTiempo() ):
+					backup = listCantiIncomp[i]
+					listCantiIncomp[i]=listCantiIncomp[i+1]
+					listCantiIncomp[i+1]=backup
+		i += 1
+	return listCantiIncomp
 
-def buscarPrendasCompatibles(dicIncompatible,prenda):
-	listaCompatibles=[]
-	for prendaI in dicIncompatible.keys():
-		if(prendaI not in dicIncompatible[prenda] and prenda not in dicIncompatible[prendaI] ):
-			listaCompatibles.append(prendaI)
-	return listaCompatibles
 
-def filtrarCompatiblesEntreSi(listaComp,dicIncompatible):
-	listaComptEntreSi = []
-	for prenda in listaComp:
-		if( prendaCompatibleEnListaPrendas(prenda,listaComptEntreSi,dicIncompatible)):
-			listaComptEntreSi.append(prenda)
-	return listaComptEntreSi
+def asignarLavados(listaIncomp,listCantiIncomp):
+	nroLavado = 1
+	prendasLavadas = []
+	listaLavados = []
+	seLavoLaPrenda = False
 
-#Funcion auxiliar dado una prenda especifica vemos si es compatible para estar 
-#en el mismo lavado que las prendas de la lista.
-def prendaCompatibleEnListaPrendas(prendaEspecifica,listaPrendas,dicIncompatible):
-	esCompatible = True
-	#print(prendaespecifica)
-	j = 0
-	for prenda in listaPrendas:
-		if(prendaEspecifica in dicIncompatible[prenda]):
-			esCompatible = False
-	return esCompatible
+	while(len(prendasLavadas)<len(listaIncomp)):
+		if( len(prendasLavadas)==0 ):#Realizamos el primer lavado para que tenga sentido recorrer la lista de lavados
+			listaLavados.append(Lavado(nroLavado)) 
+			prenda = listCantiIncomp[0]
+			listaLavados[0].agregarPrendaAlLavado(prenda)
+			prendasLavadas.append(prenda)
+			nroLavado += 1
+		else:
+			for prenda in listCantiIncomp:
+				for lavado in listaLavados:
+					if(lavado.sePuedeAgregarPrenda(prenda) and prenda not in prendasLavadas):
+						lavado.agregarPrendaAlLavado(prenda)
+						prendasLavadas.append(prenda)
+						seLavoLaPrenda = True
+				if(not seLavoLaPrenda and prenda not in prendasLavadas ):
+					listaLavados.append( Lavado(nroLavado))
+					listaLavados[nroLavado-1].agregarPrendaAlLavado(prenda)
+					prendasLavadas.append(prenda)
+					nroLavado += 1
+				seLavoLaPrenda = False
+	return listaLavados
 
-def escribirArchivo(DicLavado):
+def escribirArchivo(listaLavados):
 	archivo = open("respuestas2.txt","w")
-	for lavado,listaPrendas in DicLavado.items():
-		for prenda in listaPrendas:
-			if(lavado<len(DicLavado)):
-				archivo.write(str(prenda) + " " + str(lavado)+"\n")
-			else:
-				archivo.write(str(prenda) + " " + str(lavado))
+	for lavado in listaLavados:
+		for prenda in lavado.getListaPrendas():
+				if(lavado.getNroLavado() < len(listaLavados) ):
+					archivo.write(str(prenda.getNro()) + " " + str(lavado.getNroLavado() )+"\n")
+				else:
+					archivo.write(str(prenda.getNro()) + " " + str(lavado.getNroLavado() ))
 	archivo.close()
 
-def obtenerDicCompatibles(dicIncompatible):
-	dicCompatibles= {}
-	listaSinInncompEntreSi = []
-	for prenda in dicIncompatible.keys():
-		listaSinInncompEntreSi = buscarPrendasCompatibles(dicIncompatible,prenda)
-		dicCompatibles[prenda] = filtrarCompatiblesEntreSi(listaSinInncompEntreSi,dicIncompatible)
-	return dicCompatibles
-
-def añadirRestricciones(dicCompatibles,prob,visitaPrendaVAR,ListPrendasVAR,dicTiempos):
-	sumaAux = 0
-	aux = 1
-	for ListasPrendas in dicCompatibles.values():
-		for prendas in ListasPrendas:
-			sumaAux += ListPrendasVAR[prendas-1]
-		prob +=sumaAux == visitaPrendaVAR[aux-1]
-		#prenda = prendaQueMasTardaEnLavar(dicTiempos)
-		#prob += visitaPrendaVAR[prenda-1] == 1  #Asigno 1 a la variable de la prenda que mas tarda en lavar				
-		sumaAux =0
-		aux +=1
-
-def asignarLavado(dicCompatibles,prendasSeleccionadas):
-	dicNuevo = {}
-	lavado = 1
-	listaLavado = []
-	prendasLavadas = []
-	for prendaSelec in prendasSeleccionadas:
-		for prenda in dicCompatibles[prendaSelec]:
-			if(prenda not in prendasLavadas):
-				prendasLavadas.append(prenda)
-				listaLavado.append(prenda)
-		if(len(listaLavado)>0):
-			dicNuevo[lavado]=listaLavado
-			listaLavado=[]
-			lavado+=1
-
-	return dicNuevo,len(prendasLavadas)		
-
-
-
 def main():
-	dicIncompatible,dicTiempos = leerArchivos()
-	dic = obtenerDicCompatibles(dicIncompatible)
-	VisitoListaPrendas = []
-	tope = len(dic)
-	PrendasVisitadas = []
-	#Se uso pulp para intentar resolver el problema. 
-	prob = pulp.LpProblem("prendasl",pulp.LpMaximize) #Queremos maximizar
-	
-	for i in range(0,tope):
-		VisitoListaPrendas.append('y'+str(i+1))	
-	#y_i : Se visito la lista de la prenda compatibles de la prenda i 
-	#y: es una lista que cotiene las 385 posibles listas para visitar
-	y = [ pulp.LpVariable(i,lowBound=0,cat='Binary') for i in VisitoListaPrendas ] 
-	
+	listaIncomp = leerArchivos()
+	listaCantiIncomp =  listaPrendasConMasIncomp(listaIncomp)
+	listaLavados = asignarLavados(listaIncomp,listaCantiIncomp)
+	escribirArchivo(listaLavados)
 
-
-	for i in range(0,tope):
-		PrendasVisitadas.append('v'+str(i+1))
-	#v_i: Se visito (lavo) la prenda i 
-	v = [ pulp.LpVariable(i,lowBound=0,cat='Binary') for i in PrendasVisitadas ]
-
-	z = 0 #Funcion Objetivo
-	for i in range(0,385):
-		z += v[i]*1
-	prob += z 
-	añadirRestricciones(dic,prob,v,y,dicTiempos)
-
-	status = prob.solve() #Se resuelve el modelo 
-	lista = []
-	#Lo siguiente es para ver que variables toman valor 1 
-	for i in prob.variables():
-		if (i.varValue==1.0):
-			lista.append(i)
-		#print(i,i.varValue) #Vemos que variables toman valor 1
-
-	listasVisitadas = list (lista)
-	listaVisitadasPrendas =[]
-	#Nos interesa las listas que visitamos
-	for elemento in listasVisitadas:
-		nuevo = (str (elemento))
-		if ('y' in nuevo):
-			agregar = nuevo.replace('y','')
-			listaVisitadasPrendas.append(int (agregar))
-
-	dicLavados,cantPrendasLavadas = asignarLavado(dic,listaVisitadasPrendas)
-	print(dicLavados,cantPrendasLavadas)
+	#print(len(listaLavados))
+	#for lavado in listaLavados:
+	#	lavado.imprimirLavado()
 
 main()
